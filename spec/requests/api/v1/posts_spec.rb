ce9064b75c4468ee4245ecc27a1f534877605987
h10hUI <admin@hi0711.xyz>
2024-07-24 11:40:16 +0900

    test: 💍 特定ポストの `get` をテスト

--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -1,18 +1,29 @@
 require "rails_helper"
 
 RSpec.describe "PostAPI", type: :request do
-  describe "PostAPI" do
-    it "全てのポストを取得する" do
-      FactoryBot.create_list(:post, 10)
+  it "全てのポストを取得する" do
+    FactoryBot.create_list(:post, 10)
 
-      get "/api/v1/posts"
-      json = JSON.parse(response.body)
+    get "/api/v1/posts"
+    json = JSON.parse(response.body)
 
-      # リクエストが成功することを確認
-      expect(response).to have_http_status(:success)
+    # リクエストが成功することを確認
+    expect(response).to have_http_status(:success)
 
-      # 正しい数のデータが返されることを確認
-      expect(json["data"].length).to eq(10)
-    end
+    # 正しい数のデータが返されることを確認
+    expect(json["data"].length).to eq(10)
+  end
+
+  it "特定のポストを取得する" do
+    post = create(:post, title: "test-title")
+
+    get "/api/v1/posts/#{post.id}"
+    json = JSON.parse(response.body)
+
+    # リクエストが成功することを確認
+    expect(response).to have_http_status(:success)
+
+    # 要求した特定ポストが返されることを確認
+    expect(json["data"]["title"]).to eq("test-title")
   end
 end
